8ef071e4176fd46b96dc979f286043f4d66b32aa
zhouzhou <1766578925@qq.com>
2018-01-24 21:11:28 +0800

    http

---
+++ src/config/const/index.js
@@ -0,0 +1,6 @@
+const apiIp = 'localhost'
+const apiPort = '8081'
+const BASE_URL = 'http://' + apiIp + ':' + apiPort + '/api/'
+const SUBSCRIBE_URL = 'ws://' + apiIp + ':61614'
+
+export {BASE_URL, SUBSCRIBE_URL}
--- src/config/http/index.js
+++ src/config/http/index.js
@@ -1,1 +1,14 @@
 import axios from 'axios'
+import {BASE_URL as baseUrl, SUBSCRIBE_URL as subscribeUrl} from '../const'
+
+const instance = axios.create()
+
+class Http {
+  static install(Vue) {
+    Vue.prototype.$httpGet = Http._get
+  }
+
+  static _get(url, params) {
+    return new Promise (resolve, reject)
+  }
+}
