8d7be126f536af2b1f9bbcb34fb0d86b9f089bac
zhoupeng <zhoupeng@niunion.com>
2019-08-27 15:57:25 +0800

    vue0ssr

---
+++ vue-meta-info-example/example/views/bar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      this is bar page
+  </div>
+</template>
+
+<script>
+  export default {
+    metaInfo: {
+      title: 'bar',
+      meta: [{
+        name: 'keyWords',
+        content: 'bar test'
+      }, {
+        name: 'description',
+        content: 'this is bar description'
+      }]
+    }
+  }
+</script>
